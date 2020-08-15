423621eae33af9fd475ac98bc5d4422ae1706c43
dangwin <danghwin@gmail.com>
2020-08-15 10:33:28 -0500

    added error erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class SessionsController < ApplicationController
 
         get '/signup' do
@@ -5,11 +6,26 @@ class SessionsController < ApplicationController
         end
 
         post '/signup' do
-            user = User.new(username: params[:username], password: params[:password])
-            if user.save
-                redirect "/login"
+            @user = User.new(username: params["name"], password: params["password"])
+             @user.save
+                session[:user_id] = @user.id
+           redirect '/users/user_home'
+            
+    end 
+
+        get '/login' do 
+            erb :'/session/login'
+        end
+
+        post '/login' do
+            @user = User.find_by(username: params[:username], password: params[:password])
+            if @user 
+                session[user_id] = @user.id 
+                redirect '/users/user_home'
             else 
                 erb :"/session/error"
-            end
-    end 
+        end
+    end
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,5 @@ require 'pry'
 class Restaurant < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
-    # binding.pry
 
 end 
